9e8ae7727daa218c3021fdd374839edb9ff9cb57
Walker Neudorff <walkerneudorff@gmail.com>
2023-12-10 17:24:34 -0600

    Added back button on SearchResults page, cleaned up more code, added more notes

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,10 +1,15 @@
 import { useSelector } from "react-redux";
 import MovieItem from "../MovieItem/MovieItem";
 import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
 
 function SearchResults() {
   const searchResults = useSelector((store) => store.searchResults);
-
+  const history = useHistory();
+  const goBack = () => {
+    history.push('/');
+  }
   return (
     <main>
       <Typography gutterBottom variant="h5" display="block" mt={5} mb={3}>
@@ -14,6 +19,9 @@ function SearchResults() {
         {searchResults.map((movie) => {
           return <MovieItem key={movie.id} movie={movie} />;
         })}
+        <Button variant="contained" color="primary" onClick={goBack}>
+          BACK
+        </Button>
       </section>
     </main>
   );
